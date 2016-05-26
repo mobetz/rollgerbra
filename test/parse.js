11b56d1a8538e5d5d0cb43ef3918623a9bbc968a
Matthew Obetz <mobetz@gmail.com>
2016-05-26 11:54:18 -0400

    Rolling parse test

--- test/parse.js
+++ test/parse.js
@@ -24,5 +24,19 @@ module.exports = {
             test.equals(result.calculation, -11);
             test.done();
         }
+    },
+    "dice": {
+      "Rolling": function (test) {
+          var num_dice = 10;
+          var num_sides = 6;
+          var result = parse( num_dice + "d" + num_sides);
+          var all_dice = result.dice.reduce((a, d) => a.concat(...d), []);
+          var no_values_over_sides = all_dice.every((d) => d.raw_value <= num_sides );
+          var dice_total = all_dice.reduce((s, d)=> s+d, 0);
+          test.equals(all_dice.length, num_dice, "The wrong number of dice were rolled.");
+          test.ok(no_values_over_sides, "Some of the dice rolled have too many sides.");
+          test.equals(result.calculation, dice_total, "The returned sum does not match the sum of the returned dice.");
+          test.done();
+      }
     }
-}
+};
